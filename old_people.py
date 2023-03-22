8064dfd992c2d2ffd3ea60fbab3ebf1db60b60bc
GraciieK <122478260+GraciieK@users.noreply.github.com>
2023-03-21 21:40:26 -0400

    Lab 7 Finished
    Complete

---
+++ old_people.py
@@ -0,0 +1,91 @@
+"""
+Description:
+ Prints the name and age of all people in the Social Network database
+ who are age 50 or older, and saves the information to a CSV file.
+
+Usage:
+ python old_people.py
+"""
+ ## LOOK AT LAB 4
+import os
+import inspect 
+import sqlite3
+import pandas as pd
+from datetime import datetime
+from faker import Faker
+from pprint import pprint
+
+def main():
+    global db_path
+    script_dir = get_script_dir()
+    db_path = os.path.join(script_dir, 'social_network.db')
+
+    # Get the names and ages of all old people
+    old_people_list = get_old_people()
+
+    # Print the names and ages of all old people
+    print_name_and_age(old_people_list)
+
+    # Save the names and ages of all old people to a CSV file
+    old_people_csv = os.path.join(script_dir, 'old_people.csv')
+    save_name_and_age_to_csv(old_people_list, old_people_csv)
+
+def get_old_people():
+    """Queries the Social Network database for all people who are at least 50 years old.
+
+    Returns:
+        list: (name, age) of old people 
+    """
+    # TODO: Create function body
+
+    con = sqlite3.connect('social_network.db')
+    cur = con.cursor()
+    
+    find_old_people = """
+        SELECT name, age FROM people
+        WHERE age > 49
+        """
+    
+    cur.execute(find_old_people)
+    old_like_jeremy_quary = cur.fetchall()
+    con.close()
+
+    return (old_like_jeremy_quary)
+
+def print_name_and_age(name_and_age_list):
+    """Prints name and age of all people in provided list
+
+    Args:
+        name_and_age_list (list): (name, age) of people
+    """
+    # TODO: Create function body
+    for name, age in name_and_age_list:
+        print(f'{name} is {age} years old.')
+         
+    return
+
+def save_name_and_age_to_csv(name_and_age_list, csv_path):
+    """Saves name and age of all people in provided list
+
+    Args:
+        name_and_age_list (list): (name, age) of people
+        csv_path (str): Path of CSV file
+    """
+    # TODO: Create function body
+    df = pd.DataFrame(name_and_age_list, columns=['Name', 'Age'])
+    df.to_csv(csv_path, index=False)
+
+    
+    return
+
+def get_script_dir():
+    """Determines the path of the directory in which this script resides
+
+    Returns:
+        str: Full path of the directory in which this script resides
+    """
+    script_path = os.path.abspath(inspect.getframeinfo(inspect.currentframe()).filename)
+    return os.path.dirname(script_path)
+
+if __name__ == '__main__':
+   main()
